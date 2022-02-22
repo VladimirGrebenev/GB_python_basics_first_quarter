687a9a84cd452a95f16e666726c1469105d062b9
VladimirGrebenev <grebenev-81@mail.ru>
2022-02-22 12:02:46 +0300

    GB_python_basics_first_quarter dz_1_1 ready

--- grebenev_vladimir_dz_1_1.py
+++ grebenev_vladimir_dz_1_1.py
@@ -16,3 +16,22 @@
 #
 # Примечание: можете проверить себя, подумайте, можно ли использовать цикл для проверки работы
 # кода сразу для нескольких значений продолжительности, будет ли тут полезен список?
+from time import strftime, gmtime
+
+duration = input('Введите продолжительности времени через запятую: ')
+duration_list = []
+[duration_list.append(el.strip()) for el in duration.split(',')]
+format_time_dict = {}
+
+for el in duration_list:
+    if int(el) < 60:
+        format_time_dict[el] = strftime("%S сек", gmtime(int(el)))
+    elif int(el) > 60 and int(el) < 3600:
+        format_time_dict[el] = strftime("%M мин %S сек", gmtime(int(el)))
+    elif int(el) > 3600 and int(el) < 86400:
+        format_time_dict[el] = strftime("%H час %M мин %S сек", gmtime(int(el)))
+    else:
+        format_time_dict[el] = strftime("%d дн %H час %M мин %S сек", gmtime(int(el)-86400))
+
+for key, value in format_time_dict.items():
+    print(f'{key}\n{value}')
