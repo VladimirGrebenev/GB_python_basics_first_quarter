b51b0c26703848983fdc26866af81610a3ff018b
VladimirGrebenev <grebenev-81@mail.ru>
2022-03-01 12:01:01 +0300

    GB_python_basics_first_quarter dz_2 ready! Изменил ДЗ №2_3 и №2_5 - использую форматирование строк и isdigit()

--- grebenev_vladimir_dz_2/grebenev_vladimir_dz_2_5.py
+++ grebenev_vladimir_dz_2/grebenev_vladimir_dz_2_5.py
@@ -21,14 +21,8 @@ def price_list(price):
         n_list = str(el).split('.')
         if len(n_list) < 2:
             print(f'{n_list[0]} руб 00 коп, ', end='')
-        elif n_list[1].startswith('0') and len(n_list[1]) < 2:
-            print(f'{n_list[0]} руб {n_list[1]}0 коп, ', end='')
-        elif n_list[1].startswith('0'):
-            print(f'{n_list[0]} руб {n_list[1]} коп, ', end='')
-        elif int(n_list[1]) <= 9:
-            print(f'{n_list[0]} руб {n_list[1]}0 коп, ', end='')
         else:
-            print(f'{n_list[0]} руб {n_list[1]} коп, ', end='')
+            print(f'{n_list[0]} руб {int(n_list[1]):02} коп, ', end='')
 
 price_list(my_price)
 my_price.sort()
