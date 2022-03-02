a6e111eb8e5fcab97853149995f15b2c79ca76d4
VladimirGrebenev <grebenev-81@mail.ru>
2022-03-02 20:58:38 +0300

    GB_python_basics_first_quarter dz_3 готово. Победил задание *3.4, долго тупил с правильным размещением в цикле обнуления подсловаря и перезаписи главного словаря.

--- grebenev_vladimir_dz_3/grebenev_vladimir_dz_3_4.py
+++ grebenev_vladimir_dz_3/grebenev_vladimir_dz_3_4.py
@@ -23,27 +23,25 @@
 from pprint import pprint
 
 def thesaurus_adv(*args):
-    """функция принимает имена сотрудников, возвращает словарь типа 'И':[
-    'Илья', 'Иван']"""
-    name_dict = {}
+    """принимает имена, возвращает словарь типа {'А': {'П': ['Петр Алексеев']}"""
+    name_dict = {} # словарь с ключами по Фамилии
     for el in args:
-        key_1 = el.split(' ')[1][0]
-        name_dict[key_1] = (
-            list(filter(lambda n: n.split(' ')[1][0].startswith(key_1), args)))
+        key_1 = el.split(' ')[1][0] # буква Фамилии
+        # заполняем словарь с ключами по Фамилии
+        name_dict[key_1] = (list(filter(lambda n: n.split(' ')[1][0].startswith(key_1), args)))
 
-    sub_dict = {}
-    for value in name_dict.values():
-        for el in value:
-            key_1 = el.split(' ')[0][0]
-            sub_dict[key_1] = (
-                list(filter(lambda n: n.split(' ')[0][0].startswith(key_1), value)))
-            print(sub_dict)
+    # перебираем словарь с ключами по Фамилии для замены значений подсловарями
+    for key, value in name_dict.items():
+        sub_dict = {} # пoдcлoвapь с ключом по букве Имени
+        for el in value: #перебор значений списка по именам
+            key_2 = el.split(' ')[0][0] # буква Имени
+            #создание списка отфильтрованного по букве Имени
+            #создание словаря с ключём по букве Имени и значением списка имён по букве Имени
+            sub_dict[key_2] = (list(filter(lambda n: n.split(' ')[0][0].startswith(key_2), value)))
+        name_dict[key] = sub_dict
 
     return name_dict
 
+#сортировка по ключам - параметр sort_dicts=True установлен по умолчанию в pprint
 pprint(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев",
                      "Илья Иванов", "Анна Савельева"))
-
-
-
-
