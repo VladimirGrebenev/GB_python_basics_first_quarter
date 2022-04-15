242277e7be7ef09dc21263001abe8cc815cde01e
VladimirGrebenev <grebenev-81@mail.ru>
2022-04-15 20:20:57 +0300

    GB_python_basics_first_quarter dz_11 готовы задачи 1-6

--- grebenev_vladimir_dz_11/grebenev_vladimir_DZ_11_4.py
+++ grebenev_vladimir_dz_11/grebenev_vladimir_DZ_11_4.py
@@ -1,10 +1,18 @@
-# Домашняя работа к уроку №11 задание 4
+# Домашняя работа к уроку №11 задание 4-6
 # Начните работу над проектом «Склад оргтехники». Создайте класс, описывающий склад.
 # А также класс «Оргтехника», который будет базовым для классов-наследников.
 # Эти классы — конкретные типы оргтехники (принтер, сканер, ксерокс).
 # В базовом классе определите параметры, общие для приведённых типов.
 # В классах-наследниках реализуйте параметры, уникальные для каждого типа оргтехники.
-
+# Продолжить работу над первым заданием. Разработайте методы, которые отвечают за приём оргтехники
+# на склад и передачу в определённое подразделение компании. Для хранения данных о наименовании и
+# количестве единиц оргтехники, а также других данных, можно использовать любую подходящую структуру
+# (например, словарь).
+# Продолжить работу над вторым заданием. Реализуйте механизм валидации вводимых пользователем данных.
+# Например, для указания количества принтеров, отправленных на склад, нельзя использовать строковый
+# тип данных.
+# Подсказка: постарайтесь реализовать в проекте «Склад оргтехники» максимум возможностей, изученных
+# на уроках по ООП.
 
 class Storage:
     def __init__(self, storage_name):
@@ -14,8 +22,14 @@ class Storage:
     def __str__(self):
         return f'{self.equipment}'
 
+    @staticmethod
+    def valid_quantity(qty):
+        qty = int(qty)
+        return qty
+
     def accept_to_storage(self, eq_item, qty):
         '''приём/закупка оборудования в подразделение/на склад'''
+        qty = self.valid_quantity(qty)
         if eq_item in self.equipment:
             self.equipment[eq_item] += qty
         else:
@@ -24,6 +38,7 @@ class Storage:
 
     def move_to_department(self, where_to, eq_item, qty):
         '''перемещение обрудования между подразделениями'''
+        qty = self.valid_quantity(qty)
         if eq_item in self.equipment and (self.equipment[eq_item] - qty) >= 0:
             self.equipment[eq_item] -= qty
             where_to.accept_to_storage(eq_item, qty)
@@ -63,7 +78,7 @@ x = Xerox('Xerox 4580', 'не МФУ')
 big_storage = Storage('Big Storage')
 workshop = Storage('Workshop №321')
 big_storage.accept_to_storage(p, 20)
-workshop.accept_to_storage(p, 20)
+workshop.accept_to_storage(p, '20')
 big_storage.move_to_department(workshop, p, 10)
 big_storage.report_storage()
 big_storage.move_to_department(workshop, p, 20)
